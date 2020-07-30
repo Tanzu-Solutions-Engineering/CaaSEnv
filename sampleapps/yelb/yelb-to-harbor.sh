bbad358d27185f10a1f5179727dcc624ee88dd40
Brian Ragazzi <brianpragazzi@gmail.com>
2020-07-30 09:45:59 -0700

    Update yelb-to-harbor.sh

--- sampleapps/yelb/yelb-to-harbor.sh
+++ sampleapps/yelb/yelb-to-harbor.sh
@@ -3,7 +3,7 @@
 docker pull mreferre/yelb-ui:0.5
 docker pull redis:4.0.2
 docker pull mreferre/yelb-db:0.5
-docker pull mreferre/yel-appserver:0.5
+docker pull mreferre/yelb-appserver:0.5
 
 docker tag mreferre/yelb-ui:0.5 harbor-ctrl.caas.pez.pivotal.io/library/yelb-ui:0.5
 docker tag redis:4.0.2  harbor-ctrl.caas.pez.pivotal.io/library/redis:4.0.2
