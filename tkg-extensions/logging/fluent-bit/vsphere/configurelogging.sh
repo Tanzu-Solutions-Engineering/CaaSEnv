5f39385b73db8bdc11603db4dc8cba623fef64fd
Brian Ragazzi <brianpragazzi@gmail.com>
2020-11-20 12:01:14 -0600

    update tkg-extensions

--- tkg-extensions/logging/fluent-bit/vsphere/configurelogging.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f 00-fluent-bit-namespace.yaml
-kubectl apply -f 01-fluent-bit-service-account.yaml
-kubectl apply -f 02-fluent-bit-role.yaml
-kubectl apply -f 03-fluent-bit-role-binding.yaml
-kubectl apply -f output/http/04-fluent-bit-configmap.yaml
-kubectl apply -f output/http/05-fluent-bit-ds.yaml
